4865a1b58d318973c2133475f73681f2338563bf
Muhammad Talha <tlhmajid@gmail.com>
2026-07-15 00:07:15 +0500

    new products

--- seed_products.py
+++ seed_products.py
@@ -16,10 +16,14 @@ products = [
     ("8964000061534", "Young's Chicken Spread",310, 350, "chicken-spread.png", 20),
     ("6920247173227", "Rhode Lip tint",500, 500, "rhode-lip-tint.png", 20),
     ("8964003594084", "Peek Frean's Cake Up", 40, 40, "cupcake.png", 30),  
-    ("8961003652318", "Oreo Chocolate", 30, 30, "oreo.png", 30),
+    ("8961003652318", "Oreo Chocolate Biscuit", 30, 30, "oreo.png", 30),
     ("8961003679759", "LU Candi Biscuit", 40, 25, "candi.png", 30),
-    
-      
+    ("PL159865-200-999-L", "J. Starry Midnight Body Spray", 650, 250, "jdot.png", 10),
+    ("8964002346899", "LAYS FRENCH CHEESE", 70, 30, "lays.png", 20),
+    ("1234567890128", "ACNE CONTROL FACE WASH", 150, 140, "acne.png", 20),
+    ("8964004770616", "PEANUT PIK Biscuit", 70, 30, "peanut_biscuits.png", 20),
+    ("8964000346549", "OLPERS Milk 250ml", 90, 260, "olpers.png", 20),
+    ("8961003629198", "LU Tuc Biscuit", 50, 70, "lu.png", 40),
 ]
 
 # The Upsert Query
